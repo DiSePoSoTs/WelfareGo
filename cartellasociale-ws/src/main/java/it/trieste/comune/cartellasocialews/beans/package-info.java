ad18becd548a708069a789cb8043ad0cd76f22fd
Giulio <giulio.gobbo@comune.trieste.it>
2024-10-05 22:00:46 +0200

    tolte cose da casa

--- cartellasociale-ws/src/main/java/it/trieste/comune/cartellasocialews/beans/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://cartellasociale.sanita.insiel.it", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package it.trieste.comune.cartellasocialews.beans;
